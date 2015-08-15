00a199c0b847a1909f29f4d916823a08504164c5
Miquel Espi <espi.miquel@lab.ntt.co.jp>
2015-08-15 20:17:51 +0900

    added all the intial files

---
+++ main.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hyangsun and Miquel</title>
+
+<style type="text/css">
+<!--
+body {
+  font-size: 16px;
+	padding: 30px 30px 30px 100px;           /* required to "hide" distance div */
+	margin: 0;            /* required to "hide" distance div */
+  font-family: Trebuchet MS1, Helvetica, Meiryo UI, MS UI Gothic, sans-serif;
+
+}
+
+h1, h2, h3, body {
+  font-size: 17px;
+}
+
+h1 {
+  margin: 0;
+  padding 0;
+}
+
+h2 {
+  font-weight: normal;
+  display: inline;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  padding-top:10px;
+}
+
+ul li {
+  list-style: none;
+  padding: 0px 0px 0px 0px;
+  margin 0px;
+  display: inline;
+}
+
+a {
+  text-decoration:none;
+  color: #070;
+}
+
+a:hover {
+  color: #666;
+}
+-->
+</style>
+
+</head>
+<body>
+
+  <h1><a href="/">HyangsunAndMiquel.longnap.com</a></h1>
+
+  <ul>
+    <li><h2>This site was originally created as a "hub" to all things related to Hyangsun and Miquel's wedding. They got happily married on August 23, 2014, and it serves now a more general purpose with info about the couple.</h2></li>
+  </ul>
+
+  <ul>
+    <li><h2>hyangsun</h2>:</li>
+    <li><a href="http://longnap.com/korean/">韓国語 with Hyangsun&#x2197;</a></li>
+  </ul>
+
+  <ul>
+    <li><h2>social</h2>:</li>
+    <li><a href="http://facebook.com/hyangsunandmiquel">facebook&#x2197;</a></li>
+  </ul>
+
+  <ul>
+    <li><h2>music</h2>:</li>
+    <li><a href="http://8tracks.com/troppo/after-wedding-party-august-23-2014">wedding after-party playlist on 8tracks&#x2197;</a></li>
+<br><iframe src="http://8tracks.com/mixes/4793081/player_v3_universal" width="300" height="250" style="border: 0px none;"></iframe>
+<p class="_8t_embed_p" style="font-size: 11px; line-height: 12px;"><a href="?utm_medium=trax_embed">After-wedding party (August 23, 2014)</a> from <a href="?utm_medium=trax_embed">troppo</a> on <a href="http://8tracks.com?utm_medium=trax_embed">8tracks Radio</a>.</p>
+
+
+  </ul>
+
+  <ul>
+    <li><h2>contact</h2>:</li>
+    <li><a href="emailto:miquel@longnap.com">miquel@longnap.com</a></li>
+  </ul>
+
+  <ul>
+    <li>&nbsp;</li>
+  </ul>
+
+  <ul>
+    <li><small>by the grace of <a href="http://longnap.com">longnap</a></small></li>
+  </ul>
+
+</body>
+</html>
